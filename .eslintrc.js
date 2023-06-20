b533f1af6c4b33b19fb405ad5f80291ed1b576e0
Roman Basharin <hellekrb@yandex.ru>
2023-06-20 19:40:08 +0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'import',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    indent: 'warn',
+    'comma-dangle': 'warn',
+    'linebreak-style': ['error', 'unix'],
+    quotes: 'warn',
+    semi: ['warn', 'never'],
+    'no-multiple-empty-lines': ['warn', {
+      max: 1,
+      maxBOF: 0,
+    }],
+    'space-in-parens': 'warn',
+    'no-multi-spaces': 'warn',
+    'comma-spacing': 'warn',
+    'key-spacing': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-plusplus': ['error', {
+      allowForLoopAfterthoughts: true,
+    }],
+    'arrow-parens': [
+      'warn',
+      'as-needed',
+    ],
+    'array-bracket-spacing': 'warn',
+    'object-curly-newline': 'warn',
+    'object-curly-spacing': 'warn',
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: 'multiline-block-like', next: '*' },
+      { blankLine: 'any', prev: '*', next: ['if', 'for', 'return'] },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: [
+          'throw',
+          'try',
+          'while',
+          'do',
+          'switch',
+          'function',
+          'multiline-const',
+        ],
+      },
+      { blankLine: 'always', prev: 'multiline-const', next: '*' },
+    ],
+  },
+}
